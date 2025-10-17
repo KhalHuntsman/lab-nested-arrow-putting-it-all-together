3eeb0dca0a637ecd4ea87d3a8e423ac09995c1dd
khal_huntsman <hunter.jude.steele@gmail.com>
2025-10-16 23:27:03 -0500

    Made edits to index.js as per lab instructions

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
 
+  return (passwordAttempt) => {
+    attemptCount++;
+
+    // Account lock check
+    if (attemptCount > maxAttempts) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    // Password check
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
 
 
 module.exports = {
